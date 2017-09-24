0b9f90ff9695b1bbd4ce55cbd676291311219e3e
mm7490 <muellnermarco@gmail.com>
2017-09-24 15:07:30 -0700

    Further Basic implementation

--- Background.py
+++ Background.py
@@ -1,7 +1,8 @@
 import numpy as np
+import pyDiamonds as diamonds
 
 class Background:
-    def __init__(self,model,data,resultsPath,backgroundHyperParameters,nyquistFrequency,
+    def __init__(self,kicID,model,data,resultsPath,backgroundHyperParameters,nyquistFrequency,
                  xMeansConfiguratingParameters = None,nsmConfiguringParameters = None):
         self.backgroundModel = model
         self.data = data
@@ -19,6 +20,11 @@ class Background:
             self._nsmConfiguringParameters = nsmConfiguringParameters
         else:
             self._nsmConfiguringParameters = self.nsmcDefault()
+
+        self._uniformPriors = self.uniformPriorsObject(backgroundHyperParameters[0],backgroundHyperParameters[1])
+        self._likelihood = diamonds.ExponentialLikelihood(data[1],model)
+        self._kMeansClusterer = diamonds.KmeansClusterer(diamonds.EuclideanMetric(),xMeansConfiguratingParameters[0],
+                                                         xMeansConfiguratingParameters[1],10,0.01)
 
     @property
     def backgroundModel(self):
@@ -40,5 +46,9 @@ class Background:
         pass
 
     def nsmcDefault(self):
-        pass
+        return np.array([500,500,50000,1500,50,2.10,0.01,0.001])
+
+
+    def uniformPriorsObject(self,minParameters,maxParameters):
+        return diamonds.UniformPrior(minParameters,maxParameters)
 
