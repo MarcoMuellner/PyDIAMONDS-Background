e9176b7d8e39f80115fd4b883d4210cc2ca67f55
mm7490 <muellnermarco@gmail.com>
2017-10-02 21:06:10 +0200

    Added doctools for readthedocs

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
+from sphinx.setup_command import BuildDoc
+cmdclass = {'build_sphinx': BuildDoc}
+
+name = 'pyDiamondsBackground'
+version = '1.1'
+release = '1.1.0'
+
 
 setup(
-    name='pyDiamondsBackground',
+    name=name,
     author='Marco Muellner',
     author_email='muellnermarco@gmail.com',
-    version='1.1.0',
+    version=version,
     packages=['pyDiamondsBackground','pyDiamondsBackground/models'],
     licencse = 'MIT',
     description='An extension to pyDiamonds, intended for fitting pyDiamondsBackground signals of red giants',
@@ -13,5 +20,10 @@ setup(
     install_requires=[
         'numpy',
         'pyDiamonds'
-    ]
+    ],
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', release)}}
 )
